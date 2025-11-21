062afe2d6a9522eedbb2402292dc59fa0af42440
AlbertoSanchez82 <asancue659@g.educaand.es>
2025-11-21 19:10:18 +0100

    Update HolaMundo.java

--- EntornodeDesarrollo/src/pruebas/HolaMundo.java
+++ EntornodeDesarrollo/src/pruebas/HolaMundo.java
@@ -1,10 +1,18 @@
-package pruebas;
+package pruebas;//Indica a que clase pertenece el paquete
+				//Los paquetes se utilizzan para organizar las clases dentro de un proyecto
 
-public class HolaMundo {
+public class HolaMundo {//Aqui se declara una clase publica llamada "HolaMundo"
+						//Esta clase contendra un metodo principal del programada
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola GiT!!");
+	public static void main(String[] args) {//Metodo main es donde se incia el prgram
+											// Se ejcuta automaticamente cuando se inicia
+		
+		// TODO Auto-generated method stub //Coemtario generado por el programa
+											//Indica que se tiene que ejcutar codigo adicional si fuera necesario
+		
+		System.out.println("Hola GiT!!");//Impreme por pantalla el texto "Hola GIT!!"
+										// "println" agrega un salto de liena al final
 	}
 
 }
+
